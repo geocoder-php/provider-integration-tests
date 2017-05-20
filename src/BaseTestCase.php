37411d8fdd13e1f94f2bca7fa56f5f437bfd6646
Tobias Nyholm <tobias.nyholm@gmail.com>
2017-05-20 13:22:13 +0200

    Added a base test class (#5)
    * Added a base test class
    
    * cs

---
+++ src/BaseTestCase.php
@@ -0,0 +1,82 @@
+<?php
+
+/*
+ * This file is part of the Geocoder package.
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @license    MIT License
+ */
+
+namespace Geocoder\IntegrationTest;
+
+use GuzzleHttp\Psr7\Response;
+use Http\Client\Curl\Client as HttplugClient;
+use Http\Client\HttpClient;
+use Http\Mock\Client as MockedHttpClient;
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\ResponseInterface;
+
+/**
+ * @author Tobias Nyholm <tobias.nyholm@gmail.com>
+ */
+abstract class BaseTestCase extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * Get a real HTTP client. If $_SERVER['RESPONSE_CACHE'] is set to a path it will use cached responses.
+     *
+     * @return HttpClient
+     */
+    protected function getHttpClient($apiKey = null)
+    {
+        if (isset($_SERVER['RESPONSE_CACHE']) && false !== $_SERVER['RESPONSE_CACHE']) {
+            return new CachedResponseClient(new HttplugClient(), $_SERVER['RESPONSE_CACHE'], $apiKey);
+        } else {
+            return new HttplugClient();
+        }
+    }
+
+    /**
+     * Get a mocked HTTP client that never do calls over the internet. Use this is you want to control the response data.
+     *
+     * @param string|null $body
+     * @param int         $statusCode
+     *
+     * @return HttpClient
+     */
+    protected function getMockedHttpClient($body = null, $statusCode = 200)
+    {
+        $client = new MockedHttpClient();
+        $client->addResponse(new Response($statusCode, [], $body));
+
+        return $client;
+    }
+
+    /**
+     * Get a mocked HTTP client where you may do tests on the request.
+     *
+     * @param string|null $body
+     * @param int         $statusCode
+     *
+     * @return HttpClient
+     */
+    protected function getMockedHttpClientCallback(callable $requestCallback)
+    {
+        $client = $this->getMockBuilder(HttpClient::class)->getMock();
+
+        $client
+            ->expects($this->once())
+            ->method('sendRequest')
+            ->willReturnCallback(function (RequestInterface $request) use ($requestCallback) {
+                $response = $requestCallback($request);
+
+                if (!$response instanceof ResponseInterface) {
+                    $response = new Response(200, [], (string) $response);
+                }
+
+                return $response;
+            });
+
+        return $client;
+    }
+}
