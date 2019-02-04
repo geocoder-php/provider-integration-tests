232bbc1b261142671f4eacdc083ae39b74fd5856
Tobias Nyholm <tobias.nyholm@gmail.com>
2019-02-04 12:46:05 +0100

    Make sure we support PSR-18

---
+++ src/HttpClientTrait.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Geocoder\IntegrationTest;
+
+use Psr\Http\Client\ClientInterface;
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\ResponseInterface;
+
+/*
+ * This test is not 100% perfect. You may have installed HTTPlug:1.x and PSR-18.
+ */
+if (\interface_exists(ClientInterface::class)) {
+    /**
+     * @internal code for php-http/httplug:2.x
+     */
+    trait HttpClientTrait
+    {
+        abstract protected function doSendRequest(RequestInterface $request);
+
+        public function sendRequest(RequestInterface $request): ResponseInterface
+        {
+            return $this->doSendRequest($request);
+        }
+    }
+} else {
+    /**
+     * @internal code for php-http/httplug:1.x
+     */
+    trait HttpClientTrait
+    {
+        abstract protected function doSendRequest(RequestInterface $request);
+
+        public function sendRequest(RequestInterface $request)
+        {
+            return $this->doSendRequest($request);
+        }
+    }
+}
